db6c24200848b3e6fa58a805ef05d26880cbbc30
Dromediansk <miroslav.pillar@senacor.com>
2020-07-26 12:44:08 +0200

    GET all launches endpoint

--- api.ts
+++ api.ts
@@ -1,5 +1,6 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import * as planets from "./models/planets.ts";
+import * as launches from "./models/launches.ts";
 
 const router = new Router();
 
@@ -9,6 +10,10 @@ router.get("/", (ctx) => {
 
 router.get("/planets", (ctx) => {
   ctx.response.body = planets.getAllPlanets();
+});
+
+router.get("/launches", (ctx) => {
+  ctx.response.body = launches.getAllLaunches();
 });
 
 export default router;
